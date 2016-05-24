afaa829a7aa722b4664f3ecf9db483c16c3d9959
zhangmiaoxia <13333484236@163.com>
2016-05-24 10:34:24 +0800

    js

---
+++ laravel/resources/views/components/foot.blade.php
@@ -0,0 +1,12 @@
+<div class="m-seo-l">
+</div>
+<div style="display: none">getFriendLinkRuleTime执行时间: 0.0502</div>
+<div style="display: none">getSeoFooterRulrTime执行时间: 0.0259</div>
+<div style="display: none">SeoFooter执行时间: 0.0046</div>
+<div class="m-footer-link-list">
+    <a href="http://content.tujia.com/tujiajianjie.htm" target="_blank" class="forst" rel="nofollow">关于我们</a>|
+    <a href="http://content.tujia.com/youkebangzhu.htm" target="_blank" rel="nofollow">我是房客</a>|
+    <a href="http://content.tujia.com/qiyewenhua.htm" target="_blank" rel="nofollow">加入途家</a>|
+    <a href="http://www.tujia.com/UnitDestination/SiteMap/" target="_blank">网站地图</a>|
+    <a href="/SiteMap/Default/" target="_blank">城市地图</a>
+</div>
---
+++ laravel/resources/views/components/head.blade.php
@@ -0,0 +1,23 @@
+<div class="hd-wrap-fluid ">
+    <div class="m-hd">
+        <div class="site-logo-cont">
+            <img style="margin-top: 10px;margin-left: 17px" height="40px" width="100px" src="images/logo.jpg" alt="蜗牛logo">
+            <span class="logo-text">旅途中的家</span>
+        </div>
+        <div class="nav-list">
+            <ul>
+                <li class="first"><a href="http://localhost/web/snail/laravel/public/" class="home-link " target="_blank">首页</a></li>
+                <li><a href="http://international.tujia.com/"  target="_blank">海外·港台</a></li>
+                <li><a href="http://www.tujia.com/changzu/"  target="_blank">周租月租</a></li>
+                <li><a href="http://www.tujia.com/Experience/"  target="_blank">体验房</a></li>
+                <li><a href="http://www.tujia.com/photowall/" target="_blank" >特色房</a></li>
+
+                <li><a href="http://travel.tujia.com" target="_blank">社区</a></li>
+                <li><a href="http://www.tujia.com/EntrustTreasure/"  target="_blank">买楼收租</a></li>
+                <li><a href="http://www.tujia.com/TuYuan/"  target="_blank">途远度假</a></li>
+            </ul>
+        </div>
+
+        <div class="sub-link"><a  href="{{URL('center')}}">我的蜗牛</a></div>
+    </div>
+</div>
