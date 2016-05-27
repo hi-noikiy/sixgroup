5c0b447b3276a08027362fde0582e3425f74af22
zhangmiaoxia <13333484236@163.com>
2016-05-27 11:11:44 +0800

    center

--- laravel/resources/views/center/person.blade.php
+++ laravel/resources/views/center/person.blade.php
@@ -70,6 +70,7 @@
             <ul>
                 <li><a id="myUserInfoInfo" href="person" onclick="_gaq.push(['_trackEvent', 'pcUserInfo', '个人资料']);">个人资料</a></li>
                 <li><a id="mypassword" href="updpwd" onclick="_gaq.push(['_trackEvent', 'pcUserInfo', '修改密码']);">修改密码</a></li>
+                <li><a id="myUserInfoInfo" href="headp" onclick="_gaq.push(['_trackEvent', 'pcUserInfo', '头像上传']);">头像上传</a></li>
             </ul>
         </div>
     </div>
@@ -90,7 +91,7 @@
                                     <div class="clearfix">
                                         <div class="data-result m-fl">
                                             <a href="headp" class="usp-bg">
-                                                <img src="http://staticfile.tujia.com/bookingsite/images/user-default.png" alt="">
+                                                {{--<img src="http://staticfile.tujia.com/bookingsite/images/user-default.png" alt="">--}}
                                                 <span class="change-photo"></span>
                                             </a>
                                             @foreach($data as $v)
@@ -104,9 +105,6 @@
                                                 </div>
                                                 <div class="group-cont">
                                                     <label class="lb-txt">绑定手机：</label><strong class="lb-info">{{$v->u_phone}}</strong>
-                                                </div>
-                                                <div>
-                                                    <label class="lb-txt">注册时间：</label><strong class="lb-info">{{$v->u_time}}</strong>
                                                 </div>
                                             </div>
                                             @endforeach
